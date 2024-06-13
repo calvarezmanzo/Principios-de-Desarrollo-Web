4dc449f1117ec92404ecefcee97e60e96ecc72d5
Carlos Alvarez <carlos.alvarez@sociuschile.cl>
2024-06-12 23:12:49 -0400

    iniciar

---
+++ ejemplos/assets/js/main.js
@@ -0,0 +1,23 @@
+
+
+
+var numero=20+40;
+let numeroLet=200+400;
+const numeroConst="200"+"300";
+
+console.log(numero)
+
+console.log(numeroLet)
+
+console.log(numeroConst);
+
+
+
+
+function hola(){
+   
+    var nombre=document.getElementById("Nombre").value;
+    alert("HOLA MUNDO desde BOTON "+nombre)
+}
+
+// console.log("HOLA MUNDO")
